4d65c254ddba07097981aa3d1eaf6609e21f7234
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-11 17:04:56 +0200

    restaurants

---
+++ db/migrate/20210811122549_addref.rb
@@ -0,0 +1,5 @@
+class Addref < ActiveRecord::Migration[6.0]
+  def change
+    add_reference(:reviews, :restaurant)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chez_leo = { name: "Chez Leo", address: "7 Boundary St, London E2 7JE", phone_number: "06 99 99 99 99", category: "japanese" }
+miaou_miaou =  { name: "Miaou miaou", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "06 88 88 88 88", category: "belgian" }
+giga_kebab = { name: "Giga Kebab", address: "rue de Provence", phone_number: "06 99 77 77 77", category: "french" }
+super_kebab = { name: "Super Kebab", address: "rue de Lyon", phone_number: "06 99 77 77 76", category: "italian" }
+mega_kebab = { name: "mega Kebab", address: "rue de Toulouse", phone_number: "06 99 77 77 78", category: "chinese" }
+[chez_leo, miaou_miaou, giga_kebab, super_kebab, mega_kebab].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
